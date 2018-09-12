4f4e4475e2a88dac7fa8ffcad29c76d2ae121594
George <georgeashton95@gmail.com>
2018-09-12 17:33:22 +0100

    Initial Commit

---
+++ Another To Do List App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Another To Do List App
+//
+//  Created by George on 12/09/2018.
+//  Copyright © 2018 George Ashton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
